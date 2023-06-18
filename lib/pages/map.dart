96d46d54366ce6f0acad4cb994608296bdfb774d
nicolaslozanoc12 <nicolaslozanoc12@gmail.com>
2023-06-17 22:32:24 -0500

    se agregaron la librerias de flutter map y map caching

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 
 class Map extends StatelessWidget {
@@ -9,6 +11,12 @@ const Map({ Key? key }) : super(key: key);
     return Scaffold(
       appBar: AppBar(
         title: Text("mapa"),
+      ),
+      body: FlutterMap(
+          options: MapOptions(center: LatLng(4.0743587, -57.5861126),zoom: 10),
+          layers),
+
+
       ),
 
     );
