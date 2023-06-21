e4a9991208ce8846c5200f70caebe8527ab15992
nicolaslozanoc12 <nicolaslozanoc12@gmail.com>
2023-06-20 21:30:53 -0500

    se agregó la navegación de informacion.dart y acerca.dart

---
+++ lib/pages/informacion.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Informacion extends StatelessWidget {
+  const Informacion({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
