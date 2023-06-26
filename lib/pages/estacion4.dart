d0f6e4859ea25bd3f9a0b265fd38c23a0860e30d
NicoleBernal <90932936+NicoleBernal@users.noreply.github.com>
2023-06-26 04:12:21 -0500

    Agregando flora

--- lib/pages/estacion4.dart
+++ lib/pages/estacion4.dart
@@ -293,3 +293,100 @@ class Fauna extends StatelessWidget {
     );
   }
 }
+class Flora extends StatelessWidget {
+  const Flora({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: Container(
+            child: ListView(
+              children: [
+                Container(
+                  height: 100,
+                  margin: EdgeInsets.symmetric(vertical:70),
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/estacion2.png'),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(Colors.white.withOpacity(0.5), BlendMode.dstATop), // Agrega transparencia aquí
+
+                      )
+                  ),
+                  child: Center(
+                    child: Text("FLORA", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold), ),
+                  ),
+                ),
+
+                Container(
+                  margin: EdgeInsets.symmetric(vertical:10,horizontal: 70),
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.lightGreen),
+                    ),
+                    onPressed: (){},
+                    child: Text("Poeppigia",style: TextStyle(fontSize: 25)),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  margin: EdgeInsets.symmetric(vertical:10,horizontal: 70),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.lightGreen),
+                    ),
+                    onPressed: (){},
+                    child: Text("Scheflera",style: TextStyle(fontSize: 25)),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  margin: EdgeInsets.symmetric(vertical:10,horizontal: 70),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.lightGreen),
+                    ),
+                    onPressed: (){},
+                    child: Text("Poaceae",style: TextStyle(fontSize: 25)),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  margin: EdgeInsets.symmetric(vertical:10,horizontal: 70),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.lightGreen),
+                    ),
+                    onPressed: (){},
+                    child: Text("Zebrina",style: TextStyle(fontSize: 25)),
+                  ),
+                ),
+                Container(
+                  height: 50,
+                  margin: EdgeInsets.symmetric(vertical:10,horizontal: 70),
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.lightGreen),
+                    ),
+                    onPressed: (){},
+                    child: Text("Monstera",style: TextStyle(fontSize: 25)),
+                  ),
+                )
+
+              ],
+            ),
+
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: (){
+              Navigator.pop(context);
+            },
+            backgroundColor: Colors.lightGreen,
+            child: const Icon(Icons.exit_to_app),
+
+          )
+      ),
+    );
+  }
+}
